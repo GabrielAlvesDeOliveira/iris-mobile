e49885d4651dd11398b8da3536d2fac1dc06ef2b
decosawa <andreluiiz.gst@gmail.com>
2022-11-02 19:52:52 -0300

    Initializing the Text Results

--- src/screens/Results/TextDetectionResult/index.jsx
+++ src/screens/Results/TextDetectionResult/index.jsx
@@ -1,8 +1,41 @@
-import { Text } from "react-native";
+import { Image } from "react-native";
+import Header from "../../../components/Shared/Header/Header";
+import { TextContainer, TextContainerButton, TextContainerFilled, TextPhrasesContainer, TextPhrasesContainerFilled, TextPhrasesContainerImage, TextPhrasesLabel, TextScreen } from "./styled";
+import * as Clipboard from 'expo-clipboard';
+import { useState } from "react";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
-const TextDetectionResults = () => {
+import CopyIcon from "../../../../assets/images/copy.png"
+
+const TextDetectionResults = ({navigation, route:{params}}) => {
+
+  const [copiedText, setCopiedText] = useState('');
+
+  const copyToClipboard = async () => {
+    const text = await Clipboard.setStringAsync('texto que vai vir da deteccao')
+    setCopiedText(text)
+  }
+
   return (
-    <Text>Detecção de texto</Text>
+    <TextScreen>
+      <Header navigation={navigation}/>
+      <TextContainer>
+        <TextContainerFilled>
+         Que delícia! Comida gostosa, Ai que coisa louca, Que só de pensar, Me dá água na boca! Batata assada, Com manteiga e sal Derrete na boca - Prazer sem igual! Domingo cai bem Domingo cai bem Domingo cai bem Lá vai feijoada. Que delícia! Comida gostosa, Ai que coisa louca, Que só de pensar Me dá água na boca! Batata assada, Com manteiga e Derreta na boca - Prazer sem igual! Domingo cai bem A macarronada, No sábado - 1 hum! Lá vai feijoada.
+        </TextContainerFilled>
+        <TouchableOpacity onPress={copyToClipboard} style={!copiedText ? {backgroundColor: '#A17EFF', padding: 2, width:250,height: 50, justifyContent:"center", alignItems: "center", alignSelf: "center", borderRadius: 20, flexDirection: "row"} : {backgroundColor: '#6C55A8', padding: 2, width:250,height: 50, justifyContent:"center", alignItems: "center", alignSelf: "center", borderRadius: 20, flexDirection: "row"}}>
+          <Image source={CopyIcon} style={{width: 22, height:22}}/>
+          <TextContainerButton>
+            {!copiedText ? 'Copiar Texto' : 'Copiado'}
+          </TextContainerButton>
+        </TouchableOpacity>
+      </TextContainer>
+      <TextPhrasesLabel>Trechos</TextPhrasesLabel>
+      <TextPhrasesContainer>
+        <TextPhrasesContainerImage source={CopyIcon}/> 
+        <TextPhrasesContainerFilled>Delícia!</TextPhrasesContainerFilled>
+      </TextPhrasesContainer>      
+    </TextScreen>
   )
 }
 
--- src/screens/Results/TextDetectionResult/styled.js
+++ src/screens/Results/TextDetectionResult/styled.js
@@ -1,1 +1,66 @@
 import styled from "styled-components/native";
+
+export const TextScreen = styled.View`
+  width: 100%;
+  height: 100%;
+  background-color: ${({ theme }) => theme.colors.background.main};
+  flex-direction: column;
+  flex: 1
+`;
+
+export const TextContainer = styled.View`
+  align-self: center;
+  top: 50px;
+  border-radius: 25px;
+  box-shadow: 0px 4px 12px rgba(8, 2, 24, 0.15);
+  width: 80%;
+  background-color: ${({ theme }) => theme.colors.background.main};
+  height: 40%;
+`;
+
+export const TextContainerFilled = styled.Text`
+  padding: 20px;
+  text-align: justify;
+  word-break: break-all;
+  font-size: ${({ theme }) => theme.text.h6};
+  font-family: ${({ theme }) => theme.font.regular};
+  color: ${({ theme }) => theme.colors.typography.main};
+`;
+
+export const TextContainerButton = styled.Text`
+  padding: 5px;
+  font-size: ${({ theme }) => theme.text.h5};
+  font-family: ${({ theme }) => theme.font.semibold};
+  color: ${({ theme }) => theme.colors.background.main};
+`;
+
+export const TextPhrasesLabel = styled.Text`
+  font-size: ${({ theme }) => theme.text.h2};
+  font-family: ${({ theme }) => theme.font.bold};
+  color: ${({ theme }) => theme.colors.typography.main};
+  margin-top: 80px;
+  margin-left: 10px;
+  text-align: left;
+`;
+
+export const TextPhrasesContainer = styled.View`
+  height:50%;
+  width:80%;
+  box-shadow: 0px 4px 12px rgba(8, 2, 24, 0.15);
+  border-radius: 20px;
+  align-selft:center;
+`
+
+export const TextPhrasesContainerImage = styled.Image`
+  height: 10%;
+  width: 30%;
+  border-radius: 20px;
+`
+
+export const TextPhrasesContainerFilled = styled.Text`
+  width: 60%
+  margin-top: 5px;
+  font-size: ${({ theme }) => theme.text.h5};
+  font-family: ${({ theme }) => theme.font.regular};
+  color: ${({ theme }) => theme.colors.typography.main};
+`
