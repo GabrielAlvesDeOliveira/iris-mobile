88d3ecb056aef855d9ed10466c3ac0a48ad4852c
André Luiz Teixeira <62211442+decosawa@users.noreply.github.com>
2022-11-02 18:19:22 -0300

    Creating the Results Screen of Celebrity Detection (#33)

--- src/screens/Results/CelebrityRecognitionResult/index.jsx
+++ src/screens/Results/CelebrityRecognitionResult/index.jsx
@@ -1,9 +1,160 @@
-import { Text } from "react-native";
+import { Text, View, Dimensions, Image } from "react-native";
+import { useState } from "react";
+import * as WebBrowser from "expo-web-browser";
 
-const CelebrityRecognitionResult = () => {
+import Header from "../../../components/Shared/Header/Header";
+import LoadingScreen from "../../../components/Layout/Loading";
+
+import api_client from "../../../config/api_client";
+import formatPercentage from "../../../utils/formatPercentage";
+
+import Angry from "../../../../assets/images/emotions/angry.png";
+import Surprised from "../../../../assets/images/emotions/surprised.png";
+import Sad from "../../../../assets/images/emotions/sad.png";
+import Calm from "../../../../assets/images/emotions/calm.png";
+import Disgusted from "../../../../assets/images/emotions/disgusted.png";
+import Happy from "../../../../assets/images/emotions/happy.png";
+import Confused from "../../../../assets/images/emotions/confused.png";
+import Fear from "../../../../assets/images/emotions/fear.png";
+
+import {
+  CelebrityConfidenceLabel,
+  CelebrityImage,
+  CelebrityNameLabel,
+  CelebrityScreen,
+  CelebrityEmotionsLabel,
+  CelebrityEmotions,
+  CelebrityEmotionImage,
+  CelebrityEmotionsDetailedLabel,
+} from "./styled";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { useEffect } from "react";
+
+const CelebrityRecognitionResult = ({ navigation, route: { params } }) => {
+  const windowWidth = Dimensions.get("window").width;
+  const { image, imageName } = params;
+  const [results, setResults] = useState(null);
+
+  useEffect(() => {
+    api_client
+      .post("/detect-celebrity", {
+        imageName,
+      })
+      .then((res) => {
+        setResults(res.data);
+      })
+      .catch((err) => {
+        console.warn(err);
+      });
+  }, [imageName]);
+
+  function EmotionPicker(emotion) {
+    switch (emotion.Type) {
+      case "CALM":
+        return (
+          <View>
+            <CelebrityEmotionImage source={Calm} />
+            <CelebrityEmotionsDetailedLabel>
+              Calmo
+            </CelebrityEmotionsDetailedLabel>
+          </View>
+        );
+      case "HAPPY":
+        return (
+          <View>
+            <CelebrityEmotionImage source={Happy} />
+            <CelebrityEmotionsDetailedLabel>
+              Feliz
+            </CelebrityEmotionsDetailedLabel>
+          </View>
+        );
+      case "CONFUSED":
+        return (
+          <View>
+            <CelebrityEmotionImage source={Confused} />
+            <CelebrityEmotionsDetailedLabel>
+              Confuso
+            </CelebrityEmotionsDetailedLabel>
+          </View>
+        );
+      case "SAD":
+        return (
+          <View>
+            <CelebrityEmotionImage source={Sad} />
+            <CelebrityEmotionsDetailedLabel>
+              Triste
+            </CelebrityEmotionsDetailedLabel>
+          </View>
+        );
+      case "DISGUSTED":
+        return (
+          <View>
+            <CelebrityEmotionImage source={Disgusted} />
+            <CelebrityEmotionsDetailedLabel>
+              Nojo
+            </CelebrityEmotionsDetailedLabel>
+          </View>
+        );
+      case "SURPRISED":
+        return (
+          <View>
+            <CelebrityEmotionImage source={Surprised} />
+            <CelebrityEmotionsDetailedLabel>
+              Surpresa
+            </CelebrityEmotionsDetailedLabel>
+          </View>
+        );
+      case "ANGRY":
+        return (
+          <View>
+            <CelebrityEmotionImage source={Angry} />
+            <CelebrityEmotionsDetailedLabel>
+              Raiva
+            </CelebrityEmotionsDetailedLabel>
+          </View>
+        );
+      case "FEAR":
+        return (
+          <View>
+            <CelebrityEmotionImage source={Fear} />
+            <CelebrityEmotionsDetailedLabel>
+              Medo
+            </CelebrityEmotionsDetailedLabel>
+          </View>
+        );
+      default:
+        break;
+    }
+  }
+
   return (
-    <Text>Reconhecimento de celebridades</Text>
+    <CelebrityScreen>
+      {results ? (
+        <>
+          <Header navigation={navigation} />
+          <TouchableOpacity
+            onPress={() =>
+              WebBrowser.openBrowserAsync(`https://${results.text[0].Urls[1]}`)
+            }
+          >
+            <CelebrityImage source={{ uri: image }} size={windowWidth} />
+          </TouchableOpacity>
+          <CelebrityNameLabel>{results.text[0].Name}</CelebrityNameLabel>
+          <CelebrityConfidenceLabel>
+            Confiança: {formatPercentage(results.text[0].Face.Confidence)}
+          </CelebrityConfidenceLabel>
+          <CelebrityEmotionsLabel>Emoções Principais</CelebrityEmotionsLabel>
+          <CelebrityEmotions>
+            {results.emocao.map(
+              (emotion, i) => i <= 2 && EmotionPicker(emotion)
+            )}
+          </CelebrityEmotions>
+        </>
+      ) : (
+        <LoadingScreen />
+      )}
+    </CelebrityScreen>
   );
-}
+};
 
 export default CelebrityRecognitionResult;
--- src/screens/Results/CelebrityRecognitionResult/styled.js
+++ src/screens/Results/CelebrityRecognitionResult/styled.js
@@ -1,1 +1,58 @@
 import styled from "styled-components/native";
+
+export const CelebrityScreen = styled.View`
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: ${({ theme }) => theme.colors.background.main};
+  flex-direction: column;
+`;
+
+export const CelebrityImage = styled.Image`
+  width: ${({ size }) => size}px;
+  height: ${({ size }) => size + 70}px;
+  border-radius: 20px;
+`;
+
+export const CelebrityNameLabel = styled.Text`
+  font-size: ${({ theme }) => theme.text.h3};
+  font-family: ${({ theme }) => theme.font.bold};
+  color: ${({ theme }) => theme.colors.typography.main};
+  text-align: center;
+  margin-top: 20px;
+`;
+
+export const CelebrityConfidenceLabel = styled.Text`
+  font-size: ${({ theme }) => theme.text.h5};
+  font-family: ${({ theme }) => theme.font.bold};
+  color: ${({ theme }) => theme.colors.subtext.main};
+  text-align: center;
+`;
+
+export const CelebrityEmotionsLabel = styled.Text`
+  font-size: ${({ theme }) => theme.text.h2};
+  font-family: ${({ theme }) => theme.font.bold};
+  color: ${({ theme }) => theme.colors.typography.main};
+  margin-top: 30px
+  margin-left: 10px;
+`;
+
+export const CelebrityEmotionImage = styled.Image`
+  width: 70px;
+  height: 70px;
+`;
+
+export const CelebrityEmotions = styled.View`
+  flex-direction: row;
+  text-align: center;
+  margin-top: 20px
+  justify-content:space-around;
+  flex:1;
+`;
+
+export const CelebrityEmotionsDetailedLabel = styled.Text`
+  font-size: ${({ theme }) => theme.text.h6};
+  font-family: ${({ theme }) => theme.font.regular};
+  color: ${({ theme }) => theme.colors.subtext.main};
+  text-align: center;
+`;
